bc2f7d4037e5912992473c09ec1b2deaa8c189eb
Loris Bettazza <loriteck@yahoo.it>
2015-10-26 15:45:22 +0100

    Refactored some more code-animation
    - I changed the structure quite a lot, enjoy!

--- script/code-animation.js
+++ script/code-animation.js
@@ -1,13 +1,14 @@
 var CodeAnimation = (function($) {
     'use strict';
 
-    var codeCounter = 1,
+    // Variables
+    var wordCounter = 1,
         CODE = " /** \n" +
         " * Checks whether the person is motivated to learn web development or not. \n" +
         " */ \n\n" +
         " function checkMotivation() { \n" +
-        "   var you = new Human(), \n" +
-        "       motivation = you.getMotivation(), \n\n" +
+        "   var you = new Person(), \n" +
+        "     motivation = you.getMotivation(); \n\n" +
         "   if (motivation > 9000) { \n" +
         "     console.log('OMG your motivation is over 9000! Better start now!!'); \n" +
         "   } \n" +
@@ -20,45 +21,64 @@ var CodeAnimation = (function($) {
         " }",
         CODE_WORDS = CODE.split(' '),
         CODE_HIGHLIGHT = '<span class="code-color"></span>',
-        CODE_KEYWORDS = ['var', 'function', 'if', 'else', 'new'];
+        CODE_KEYWORDS = ['var', 'function', 'if', 'else', 'new'],
+        $DEFAULT_CONTAINER = $('.code-area');
+
+    // Init
+    // Replaces empty words with a space (the empty words occur when there are 2 or more consecutive spaces, so in the indentation)
+    for (var i=1; i<CODE_WORDS.length; i++) {
+        if (CODE_WORDS[i] === '') {
+            CODE_WORDS[i] = ' ';
+        }
+    }
 
     /**
     * Animates the code inside the osx window
     */
     function animateCode() {
-        var $codeContainer = $('.code-area'),
+        // If we find an undefined value, it means we don't have any more words to print
+        if (typeof CODE_WORDS[wordCounter] === 'undefined') {
+            return;
+        }
+
+        var $codeContainer = $DEFAULT_CONTAINER,
             isKeyword = false;
 
-        if (CODE_KEYWORDS.indexOf(CODE_WORDS[codeCounter]) > -1) {
+        // If the current word is a keyword
+        if (CODE_KEYWORDS.indexOf(CODE_WORDS[wordCounter]) > -1) {
             $codeContainer.append(CODE_HIGHLIGHT);
             $codeContainer = $('.code-area span:last-child');
             isKeyword = true;
         }
 
-        setTimeout(function() {
-            typeCharacter($codeContainer, (' ' + CODE_WORDS[codeCounter]), 0, isKeyword, (CODE_WORDS.length - 1));
-        }, 50);
+        printWord($codeContainer, CODE_WORDS[wordCounter]);
     }
 
     /**
-    * Simulates the text being typed
+    * Prints a word in a given container
     */
-    function typeCharacter($element, word, index, isKeyword, length) {
-        if (typeof word[index] === 'undefined') {
-            if (codeCounter !== length) {
-                codeCounter++;
+    function printWord($element, word) {
+        var wordLenght = word.length,
+            charIndex = 0;
+
+        $element.html($element.html() + ' '); // Put initial space before the word (this is why the indentation is doubled)
+        setTimeout(typeCharacter, 50);
+
+        /**
+        * Types each character of the current word with a delay
+        */
+        function typeCharacter() {
+            $element.html($element.html() + word[charIndex]);
+
+            if (charIndex >= wordLenght-1) {
+                wordCounter++;
                 animateCode();
             } else {
-                clearTimeout(); // Stop animation
+                charIndex++;
+                setTimeout(typeCharacter, 50);
             }
-        } else {
-            $element.html($element.html() + word[index]);
-            setTimeout(function() {
-                typeCharacter($element, word, ++index, isKeyword, length);
-            }, 50);
         }
     }
-
 
     // Expose methods
     return {
